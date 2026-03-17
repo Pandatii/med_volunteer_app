b0dea30a3761f147f0afa03a721975c1c19968f6
panda <pandatii@gmail.com>
2026-03-17 20:48:32 +0700

    change undefined

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 
-const FORM_URL = 'https://script.google.com/macros/s/AKfycbx_Dx0QOSrsahqb51CSH5G9NTPFVlmA9E-vc22Z6nf1CXP0unl_SfK78hFDjNn43oFpxQ/exec'
+const FORM_URL = 'https://script.google.com/macros/s/AKfycbxsKHu4hV9kmU0OQDA5KxjKkxJEqq4ItwjNduEZLhQT3kUJfH9OqFOngyjQTr0ZVHxMBA/exec'
 const RESULT_URL = 'https://script.google.com/macros/s/AKfycbxM9Uuc8yCVmiaVeC6L81_yDeXge85h2KDsWnbaVTUYs8TGgrwbUebQmRTgHq9vHwkCRw/exec' // placeholder
 
 const clinics = [
